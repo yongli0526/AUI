671c725442ef3d947d8d04acebe3d2dbf108432d
yongli0526 <yo.li@foxmail.com>
2019-06-26 22:06:13 +0800

    [AUI]add index

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+    entry: __dirname + '/JSX/index.jsx',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        historyApiFallback: {
+            index: 'index.html'
+        }
+    },
+    module: {
+        rules: [{
+            test: /\.(png|jpg)$/,
+            exclude: /node_modules/,
+            use: [
+                {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8192,
+                        name: "[name].[ext]",
+                        mimetype: 'image/png',
+                        outputPath: 'images/'
+                    },
+                }
+            ]
+        },{
+            test: /\.jsx$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }]
+    },
+    resolve: {
+        modules: ["./node_modules"],
+        extensions: [".js", ".json", ".jsx", ".css"]
+    }
+}
