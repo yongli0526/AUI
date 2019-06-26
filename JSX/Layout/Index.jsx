d54543afd5a00a382496cafaf5f0e7dc1cbe9fd0
yongli0526 <yo.li@foxmail.com>
2019-06-26 22:15:53 +0800

    [AUI]add index

---
+++ JSX/Layout/Index.jsx
@@ -0,0 +1,11 @@
+import React,{Component} from "react";
+import { render } from "react-dom";
+class Layout extends Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return<div>1</div>;
+    }
+}
+export default Layout;
--- JSX/index.jsx
+++ JSX/index.jsx
@@ -1,5 +1,5 @@
 import React from "react"
 import { render } from "react-dom"
-import Root from "./Root"
+import Layout from "./Layout/Index"
 
-render(<Root />, document.getElementById("root"))
+render(<Layout />, document.getElementById("root"))
